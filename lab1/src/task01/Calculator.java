5e73e29b781681ed5c57bad60d6034b91ed11c49
basilissk <vanasilvergoldsssk@mail.ru>
2023-09-23 21:09:02 +0300

    1st task

--- lab1/src/task01/Calculator.java
+++ lab1/src/task01/Calculator.java
@@ -1,5 +1,15 @@
 package task01;
+import java.lang.Math;
 
 public class Calculator {
+    public static float Result(float x, float y){
+        return Numerator(x,y)/Denumerator(x,y)+x;
+    }
 
+    private static float Numerator(float x, float y){
+        return (float)(1+Math.pow(Math.sin(x+y),2));
+    }
+    private static float Denumerator(float x, float y){
+        return (float)(2+Math.abs(x-(2*x/(1+x*x*y*y))));
+    }
 }
